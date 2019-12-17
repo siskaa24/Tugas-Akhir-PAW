fa3cb6808c65a80e89243cdd4b6ea68a8ae29242
meprilfanys <harunfn22@gmail.com>
2019-12-18 04:01:50 +0700

    Update sidebar.php
    penambahan pada templates sidebar

--- application/views/template_administrator/sidebar.php
+++ application/views/template_administrator/sidebar.php
@@ -8,62 +8,46 @@
 
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon ">
-        <i class="fas fa-university"></i>
+        <div class="sidebar-brand-icon">
+          <i class="fas fa-university"></i>
         </div>
-        <div class="sidebar-brand-text mx-1">SISFO-AKADEMIK </div>
-      </a>  
+        <div class="sidebar-brand-text mx-1">SISFO-AKADEMIK <sup></sup></div>
+      </a>
 
       <!-- Divider -->
       <hr class="sidebar-divider my-0">
-      <style>
-          <?php include 'css/ssb-admin-2.min.css'; ?>
-      </style>
+
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="index.html">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
+
       <!-- Nav Item - Pages Collapse Menu -->
-      
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-          <i class="fas fa-fw fa-folder"></i>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-university"></i>
           <span>Akademik</span>
         </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Sub Menu Akademik:</h6>
+            <h6 class="collapse-header">Sub-Menu Akademik:</h6>
             <a class="collapse-item" href="<?php echo base_url('administrator/jurusan') ?>">Jurusan</a>
             <a class="collapse-item" href="<?php echo base_url('administrator/prodi') ?>">Program Studi</a>
-            <a class="collapse-item" href="Informasi.html">Matakuliah</a>
-            <a class="collapse-item" href="404.html">Mahasiswa</a>
-            <a class="collapse-item" href="blank.html">Tahun Akademik</a>
-            <a class="collapse-item" href="blank.html">KRS</a>
-            <a class="collapse-item" href="blank.html">Input Nilai</a>
-            <a class="collapse-item" href="blank.html">KHS</a>
-            <a class="collapse-item" href="blank.html">Cetak Transkip</a>
-            <a class="collapse-item" href="blank.html">Dosen</a>
+            <a class="collapse-item" href="<?php echo base_url('administrator/matakuliah') ?>">Mata Kuliah</a>
+            <a class="collapse-item" href="cards.html">Mahasiswa</a>
+            <a class="collapse-item" href="buttons.html">Jurusan</a>
+            <a class="collapse-item" href="cards.html">Tahun Akademik</a>
+            <a class="collapse-item" href="buttons.html">KRS</a>
+            <a class="collapse-item" href="cards.html">Input Nilai</a>
+            <a class="collapse-item" href="buttons.html">Jurusan</a>
+            <a class="collapse-item" href="cards.html">KHS</a>
+            <a class="collapse-item" href="buttons.html">Cetak Transkip</a>
+            <a class="collapse-item" href="cards.html">Dosen</a>
           </div>
         </div>
       </li>
-
-      <!-- <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
-        </a>
-
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
-          </div>
-
-        </div>
-      </li> -->
 
       <!-- Nav Item - Utilities Collapse Menu -->
       <li class="nav-item">
@@ -73,12 +57,13 @@
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Sub Menu Pengaturan:</h6>
-            <a class="collapse-item" href="utilities-color.html">user</a>
+            <h6 class="collapse-header">Sub-Menu Pengaturan :</h6>
+            <a class="collapse-item" href="<?php echo base_url('administrator/users') ?>">User</a>
             <a class="collapse-item" href="utilities-border.html">Menu</a>
           </div>
         </div>
       </li>
+
       <!-- Nav Item - Pages Collapse Menu -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
@@ -87,23 +72,24 @@
         </a>
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Sub Menu Info Kampus:</h6>
-            <a class="collapse-item" href="Identitas.html">Identitas</a>
-            <a class="collapse-item" href="Kategori.html">Kategori</a>
-            <a class="collapse-item" href="Informasi.html">Informasi Kampus</a>
+            <h6 class="collapse-header">Sub-Menu Info Kampus :</h6>
+            <a class="collapse-item" href="login.html">Identitas</a>
+            <a class="collapse-item" href="register.html">Kategori</a>
+            <a class="collapse-item" href="forgot-password.html">Informasi Kampus</a>
             <a class="collapse-item" href="404.html">Tentang Kampus</a>
             <a class="collapse-item" href="blank.html">Fasilitas</a>
-            <a class="collapse-item" href="blank.html">Materi Perkuliahan</a>
-            <a class="collapse-item" href="blank.html">Galeri</a>
+            <a class="collapse-item" href="404.html">Materi Perkuliahan</a>
+            <a class="collapse-item" href="blank.html">Gallery</a>
             <a class="collapse-item" href="blank.html">Kontak</a>
           </div>
         </div>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url('administrator/auth/logout') ?>">
-        <i class="fas fa-sign-out-alt"></i>
+          <i class="fas fa-sign-out-alt"></i>
           <span>Logout</span></a>
       </li>
+
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
 
